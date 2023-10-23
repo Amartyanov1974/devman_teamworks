08affa1fd9bc01001ae196f345e0add17bf787d2
Amartyanov1974 <randomquake@yandex.ru>
2023-10-23 12:13:30 +0400

    Update admin.py

--- devman/admin.py
+++ devman/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from devman.models import Student, Project_manager
+
+
+@admin.register(Student)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'tg_account', 'level']
+    readonly_fields = ['name', 'tg_account']
+
+@admin.register(Project_manager)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'tg_account']
+    readonly_fields = ['name', 'tg_account']
