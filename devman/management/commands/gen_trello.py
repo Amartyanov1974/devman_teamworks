4dc27f271c7d680c3130c3eda4da678439c961ab
Amartyanov1974 <randomquake@yandex.ru>
2023-10-26 17:01:42 +0400

    Update gen_trello.py

--- devman/management/commands/gen_trello.py
+++ devman/management/commands/gen_trello.py
@@ -7,10 +7,7 @@ from devman.models import Student, ProjectManager, TeamWork
 from django.conf import settings
 
 
-
 def add_member(board_id, member_id):
-
-    # Добавление пользователя на доску
 
     url = f'https://api.trello.com/1/boards/{board_id}/members/{member_id}'
     user_type = 'normal'
@@ -21,11 +18,10 @@ def add_member(board_id, member_id):
     }
 
     response = request('PUT', url, params=query)
+    # response.raise_for_status()
 
 
 def get_member_trelloid(trello_account):
-
-    # Определение trello_id по trello_account
 
     url = f'https://api.trello.com/1/members/{trello_account}'
 
@@ -44,6 +40,7 @@ def get_member_trelloid(trello_account):
        headers=headers,
        params=query
     )
+    # response.raise_for_status()
     trello_id = json.loads(response.text)['id']
     return trello_id
 
@@ -53,6 +50,7 @@ class Command(BaseCommand):
         for teamwork in TeamWork.objects.all():
 
             # Создание доски
+
             url = 'https://api.trello.com/1/boards/'
             team_students = teamwork.student.all()
             students = []
@@ -66,17 +64,24 @@ class Command(BaseCommand):
             }
 
             response = request('POST', url, params=query)
+            # response.raise_for_status()
             board_id = json.loads(response.text)['id']
             trello_url = json.loads(response.text)['shortUrl']
             teamwork.trello_url = trello_url
             teamwork.save(update_fields=['trello_url'])
             for student in team_students:
-                if not student.trello_id:
+                if  student.trello_account and not student.trello_id:
                     print(student.trello_account)
                     trello_id = get_member_trelloid(student.trello_account)
                     student.trello_id = trello_id
                     student.save(update_fields=['trello_id'])
-                print(board_id, student.trello_id)
-                add_member(board_id, student.trello_id)
+                if student.trello_id:
+                    add_member(board_id, student.trello_id)
+            pm_trello_account = teamwork.project_manager.trello_account
+            if pm_trello_account:
+                print(pm_trello_account)
+                trello_id = get_member_trelloid(pm_trello_account)
+                add_member(board_id, trello_id)
 
-
+# Выяснить что за ошибка
+# Member not allowed to add a multi-board guest without allowBillableGuest parameter
